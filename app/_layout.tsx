43dac6dad47a240693ec8b07d8266b265d230085
Debbie Young <frenchfriesdb@gmail.com>
2026-03-02 06:38:09 -0800

    first deploymnet code rabbit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,15 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { ActivityIndicator } from "react-native";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const [fontsLoaded] = useFonts({
+    'appFont':require('./../assets/fonts/Oswald-ExtraLight.ttf'),
+    'appFontBold':require('./../assets/fonts/Oswald-Bold.ttf')
+  });
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+  if (!fontsLoaded) {
+    return <ActivityIndicator/>;
+  }
+  return <Stack />;
 }
